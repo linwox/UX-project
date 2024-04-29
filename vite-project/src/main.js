d36bfa2e439ad7fc990c93e97799f22c4514bc84
Linnea <150425979+linwox@users.noreply.github.com>
2024-04-29 20:57:32 +0200

    New try to make app with vue and vite
    Co-authored-by: adamj0425 <adamj0425@users.noreply.github.com>
    Co-authored-by: AmandaJava23 <AmandaJava23@users.noreply.github.com>

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -5,10 +5,4 @@ import RiksdagensData from "./services/RiksdagensData.js"
 import './style.css'
 import App from './App.vue'
 
-// fanns redan
 createApp(App).mount('#app')
-
-// inklistrat från vår gamla kod
-const vueApp = Vue.createApp(app)
-vueApp.component('image-card', ImageCard)
-vueApp.mount("#app")

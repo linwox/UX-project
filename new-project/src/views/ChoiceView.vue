26ef6fa7f3da154b38b967f44021f6759356a9db
Linnea <150425979+linwox@users.noreply.github.com>
2024-05-03 14:46:58 +0200

    A little css

--- new-project/src/views/ChoiceView.vue
+++ new-project/src/views/ChoiceView.vue
@@ -1,20 +1,41 @@
 <script setup></script>
 
 <template>
-    <main class="main">
-        <p>Välj att swipea på:</p>
-        <RouterLink to="/card">Bild</RouterLink>
-        <RouterLink to="/quote">Citat</RouterLink>
-    </main>
+  <main class="main">
+    <p>Välj din regering utifrån bilder eller citat:</p>
+    <button class="choice" @click="$router.push('card')">Bilder</button>
+    <button class="choice" @click="$router.push('quote')">Citat</button>
+  </main>
 </template>
 
 <script></script>
 
 <style>
 .main {
-    width: 30rem;
-    height: 40rem;
-    border: 1px solid black;
-    padding: 1rem;
+  width: 30rem;
+  height: 40rem;
+  border: 1px solid black;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
+
+p {
+  font-family: Georgia, 'Times New Roman', Times, serif;
+  font-weight: 600;
+  font-size: larger;
+}
+
+.choice {
+  width: 10rem;
+  height: 5rem;
+  font-family: Helvetica, sans-serif;
+  font-weight: 600;
+  font-size: x-large;
+  margin-top: 3rem;
+  
+}
+
 </style>
--- new-project/src/views/PickMinisterView.vue
+++ new-project/src/views/PickMinisterView.vue
@@ -1,20 +1,20 @@
 <script setup></script>
 
 <template>
-    <main class="main">
-        <h2>Ministerpost</h2>
-        <div>Välj vem som ska ha posten</div>
-        <RouterLink to="/government">Se din regering</RouterLink>
-    </main>
+  <main class="main">
+    <h2>Ministerpost</h2>
+    <div>Välj vem som ska ha posten</div>
+    <RouterLink to="/government">Se din regering</RouterLink>
+  </main>
 </template>
 
 <script></script>
 
 <style>
 .main {
-    width: 30rem;
-    height: 40rem;
-    border: 1px solid black;
-    padding: 1rem;
+  width: 30rem;
+  height: 40rem;
+  border: 1px solid black;
+  padding: 1rem;
 }
 </style>

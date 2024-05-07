e71124190b354f0d8e8217b1578d2dbf84542c85
adamj0425 <adamj0425@gmail.com>
2024-05-07 10:58:21 +0200

    Framsteg med pickminister

--- new-project/src/views/PickMinisterView.vue
+++ new-project/src/views/PickMinisterView.vue
@@ -1,9 +1,27 @@
-<script setup></script>
+<script setup>
+import { useSelectedStore } from '@/stores/selected'
+import { mapStores } from 'pinia'
+</script>
+
+<script>
+export default {
+  computed: {
+    ...mapStores(useSelectedStore, ['selectedPersons'])
+  }
+}
+</script>
 
 <template>
   <main class="main">
     <h2>Ministerpost</h2>
     <div>Välj vem som ska ha posten</div>
+    <div v-for="person of selectedStore.selectedPersons"> 
+    
+      <img :src="person.imageUrl" class="">
+      <p>{{ person.age }}</p>
+    
+    
+    </div>
 
     <RouterLink to="/government">Se din regering</RouterLink>
   </main>

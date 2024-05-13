a94f056966d59a5a2fc73e9644cc51501fd9fcc6
Linnea <150425979+linwox@users.noreply.github.com>
2024-05-13 13:46:07 +0200

    changed placements in pickminister

--- new-project/src/stores/selected.js
+++ new-project/src/stores/selected.js
@@ -1,11 +1,8 @@
 import { defineStore } from 'pinia'
-import { list } from 'postcss'
-import { ref } from 'vue'
-import { reactive } from 'vue'
 
 export const useSelectedStore = defineStore('selected', {
   state: () => ({
-    selectedPersons: ([])
+    selectedPersons: []
   }),
   actions: {
     addSelectedPersonData(id, firstName, age, party, imageUrl) {
--- new-project/src/views/PickMinisterView.vue
+++ new-project/src/views/PickMinisterView.vue
@@ -21,7 +21,8 @@ export default {
         ['migration', undefined],
         ['rural', undefined]
       ]),
-      ministerPost: undefined
+      ministerPost: undefined,
+      randomId: undefined
     }
   },
   computed: {
@@ -35,7 +36,7 @@ export default {
       }
     },
     setMinister() {
-      this.ministers.set(this.ministerPost, randomId)
+      this.ministers.set(this.ministerPost, this.randomId)
     },
     getMinisterPostValue() {
       const e = document.getElementById('minister_post')
@@ -54,72 +55,43 @@ export default {
 <template>
   <h2 class="flex items-center justify-center mt-3">Ministerpost</h2>
   <div class="flex items-center justify-center mt-3">Välj vem som ska ha posten</div>
-  <!-- <div class="flex items-center justify-center mt-3" v-for="minister of ministerLabels"> {{ minister }} </div> -->
+  <div class="flex items-center justify-center mt-3">
+    <select id="minister_post" v-model="ministerPost" class="select select-bordered w-48 max-w-xs">
+      <option disabled selected>Ministerposter</option>
+      <option value="prime">Statsminister</option>
+      <option value="finance">Finansminister</option>
+      <option value="equality">Jämställdhetsminister</option>
+      <option value="climate">Klimatminister</option>
+      <option value="health">Sjukvårdminister</option>
+      <option value="culture">Kulturminister</option>
+      <option value="school">Skolminister</option>
+      <option value="justice">Justitieminister</option>
+      <option value="defence">Försvarsminister</option>
+      <option value="foreign">Utrikesminister</option>
+      <option value="migration">Migrationsminister</option>
+      <option value="rural">Landsbygdsminister</option>
+    </select>
+  </div>
   <div class="flex items-center justify-center mt-3">
     <div class="w- carousel carousel-center p-4 space-x-4 bg-neutral rounded-box bg-white">
-
-      <!-- <div v-for="person of selectedStore.selectedPersons" class="carousel-item ml-8 l" :data-item="person.id">
-        <div>
-          <img v-bind:src="person.imageUrl" class="rounded-box border-2" />
-          <div class="block align-bottom">{{ person.firstName }} {{ person.age }}</div>
-          <p class="flex items-center justify-center content-center">
-          </p> -->
-
-      <!-- <div
-        v-for="person of selectedStore.selectedPersons"
-        class="carousel-item ml-8 l"
-        :data-item="person.id"
-      > -->
-      
-        <select
-          id="minister_post"
-          v-model="ministerPost"
-          class="select select-bordered w-full max-w-xs"
-        >
-          <option disabled selected>Ministerposter</option>
-          <option value="prime">Statsminister</option>
-          <option value="finance">Finansminister</option>
-          <option value="equality">Jämställdhetsminister</option>
-          <option value="climate">Klimatminister</option>
-          <option value="health">Sjukvårdminister</option>
-          <option value="culture">Kulturminister</option>
-          <option value="school">Skolminister</option>
-          <option value="justice">Justitieminister</option>
-          <option value="defence">Försvarsminister</option>
-          <option value="foreign">Utrikesminister</option>
-          <option value="migration">Migrationsminister</option>
-          <option value="rural">Landsbygdsminister</option>
-        </select>
-        <div class="w-72 carousel carousel-center p-4 space-x-4 bg-neutral rounded-box bg-white">
-          <div v-for="person of selectedStore.selectedPersons" class="carousel-item ml-8 l">
-            <div>
-              <img :src="person.imageUrl" alt="politician" class="rounded-box border-2" />
-              <div class="block align-bottom">{{ person.firstName }} {{ person.age }}</div>
-              <p class="flex items-center justify-center content-center">
-                {{ ministerPost }}
-              </p>
-              <p class="flex items-center justify-center content-center">
-                <button class="p-2 rounded border-2" @click="setMinister">Välj</button>
-              </p>
-            </div>
+      <div class="w-72 carousel carousel-center p-4 space-x-4 bg-neutral rounded-box bg-white">
+        <div v-for="person of selectedStore.selectedPersons" class="carousel-item ml-8 l">
+          <div>
+            <img :src="person.imageUrl" alt="politician" class="rounded-box border-2" />
+            <div class="block align-bottom">{{ person.firstName }} {{ person.age }}</div>
+            <p class="flex items-center justify-center content-center">
+              {{ ministerPost }}
+            </p>
+            <p class="flex items-center justify-center content-center">
+              <button class="p-2 rounded border-2" @click="setMinister">Välj</button>
+            </p>
           </div>
         </div>
       </div>
     </div>
-  <!-- </div> -->
+  </div>
   <div class="flex items-center justify-center mt-2">
     <button class="p-2 rounded border-2" @click="selectMinister">Välj</button>
   </div>
-  <RouterLink class="flex items-center justify-center mt-5" to="/government"
-    >Se din regering</RouterLink
-  >
+  <RouterLink class="flex items-center justify-center mt-5" to="/government">Se din regering</RouterLink>
 </template>
-
-<!-- <style>
-.main {
-  width: 30rem;
-  height: 40rem;
-  border: 1px solid black;
-  padding: 1rem;
-}
-</style> -->

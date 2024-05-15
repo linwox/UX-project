b7784e87dbe30bc22de57dc400d706233b598ccb
Amanda Vasilis <amavas0414@skola.goteborg.se>
2024-05-15 15:47:48 +0200

    when clicked yes, the person is removed from the listOfIds

--- new-project/src/views/CardView.vue
+++ new-project/src/views/CardView.vue
@@ -58,9 +58,9 @@ export default {
     async handleYesClick() {
 
       if (this.selectedStore.selectedPersons.includes(this.randomId)) {
-        const index = this.selectedStore.selectedPersons.indexOf(this.randomId)
+        const index = this.listOfIds.indexOf(this.randomId)
         if (index !== -1) {
-          this.selectedStore.selectedPersons.splice(index, 1)
+          this.listOfIds.splice(index, 1)
         }
       }
 

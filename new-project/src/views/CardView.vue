51e10dbae4dab6bafd3420070ff53863a025f764
Linnea <150425979+linwox@users.noreply.github.com>
2024-05-17 22:19:34 +0200

    re-added the quote code that i accidentally deleted in last commit, styled cardview some more

--- new-project/src/views/CardView.vue
+++ new-project/src/views/CardView.vue
@@ -10,36 +10,77 @@ import { generateQuote } from '@/lib/QuoteManager.js'
 <template>
   <header>
     <div class="bg-[url('../assets/headerBakgrund.svg')] bg-cover h-screen flex justify-center items-center">
-
       <div class="flex flex-col justify-center items-center h-screen space-y-4">
-        <div class="text-xl font-semibold">{{ count }} / 12</div>
 
-        <span v-if="loading" class="loading loading-dots loading-lg"></span>
-        <h3 v-else class="text-2xl font-semibold">Tänk att ta en kaffe med...</h3>
 
-        <div v-if="choiceStore.choice"
-          class="card relative w-80 h-2/3 md:w-1/2 flex flex-col shadow-xl border-slate border-2">
-          <p class="text-3xl text-white font-bold absolute bottom-28 left-8 z-20">
-            {{ firstName }} {{ age }}
-          </p>
+        <div v-if="choiceStore.choice" class="flex flex-col h-screen w-screen justify-center items-center gap-5">
 
-          <div class="flex items-end justify-center absolute bottom-6 left-6 right-6 z-20">
-            <div class="card-actions space-x-36 flex justify-center">
-              <button @answer="handleAnswer" @click="handleNoClick"
-                class="bg-[url('../assets/no_icon.svg')] bg-cover w-14 h-14"></button>
-              <button @answer="handleAnswer" @click="handleYesClick"
-                class="bg-[url('../assets/yes_icon.svg')] bg-cover w-14 h-14"></button>
+          <h3 class="text-2xl font-semibold">Tänk att ta en kaffe med...</h3>
+
+          <div class="card relative w-80 h-2/3 md:w-1/2 flex flex-col shadow-xl border-slate border-2">
+
+            <div v-if="loading" class="flex justify-center items-center w-full h-full">
+              <span class="loading loading-dots loading-lg"></span>
+            </div>
+
+            <div v-else>
+
+              <p class="text-3xl text-white font-bold absolute bottom-28 left-8 z-20">
+                {{ firstName }} {{ age }}
+              </p>
+
+              <div
+                class="w-full h-72 bg-gradient-to-t from-black via-transparent absolute bottom-0 left-0 right-0 rounded-2xl z-10">
+              </div>
+
+              <img :src="imageUrl" alt="Politician"
+                class="w-full h-full object-cover absolute top-0 left-0 rounded-2xl z-0" />
+
+            </div>
+
+
+            <div class="flex items-end justify-center absolute bottom-6 left-6 right-6 z-20">
+              <div class="card-actions space-x-36 flex justify-center">
+                <button @answer="handleAnswer" @click="handleNoClick"
+                  class="bg-[url('../assets/no_icon.svg')] bg-cover w-14 h-14"></button>
+                <button @answer="handleAnswer" @click="handleYesClick"
+                  class="bg-[url('../assets/yes_icon.svg')] bg-cover w-14 h-14"></button>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="text-xl font-semibold">{{ count }} / 12</div>
+
+        </div>
+
+        <div v-else class="flex flex-col h-screen w-screen justify-center items-center gap-5">
+          <h3 class="text-2xl font-semibold">Håller du med om...?</h3>
+
+          <div
+            class="card relative w-80 h-2/3 md:w-1/2 flex flex-col justify-center items-center shadow-xl border-slate border-2">
+            <div v-if="loading" class="flex justify-center items-center w-full h-full">
+              <span class="loading loading-dots loading-lg"></span>
+            </div>
+
+            <p v-else class="text-2xl text-black font-semibold p-8 text-center">{{ quote }}</p>
+
+            <div class="flex items-end justify-center absolute bottom-6 left-6 right-6 z-20">
+              <div class="card-actions space-x-36 flex justify-center">
+                <button @answer="handleAnswer" @click="handleNoClick"
+                  class="bg-[url('../assets/no_icon.svg')] bg-cover w-14 h-14"></button>
+                <button @answer="handleAnswer" @click="handleYesClick"
+                  class="bg-[url('../assets/yes_icon.svg')] bg-cover w-14 h-14"></button>
+              </div>
             </div>
           </div>
 
-          <div class="w-full h-64 bg-gradient-to-t from-black absolute bottom-0 left-0 right-0 rounded-2xl z-10"></div>
-
-          <img :src="imageUrl" alt="Politician"
-            class="w-full h-full object-cover absolute top-0 left-0 rounded-2xl z-0" />
+          <div class="text-xl font-semibold">{{ count }} / 12</div>
         </div>
-      </div>
 
       </div>
+
+    </div>
   </header>
 </template>
 
